faed4ff415e9bd25c860964041eb15dfe0fc4e2f
Rafael Gioffi <rafaelgioffi@gmail.com>
2022-10-26 22:09:01 -0300

    Updating exercises

---
+++ Exercicios_Lista11/Emails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Lista11
+{
+    class Emails
+    {
+        private string _nome { get; set; }
+        private string _email { get; set; }
+
+        public Emails (string nome, string email)
+        {
+            _nome = nome;
+            _email = email;
+        }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ Exercicios_Lista11/frmIndex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicios_Lista11
+{
+    public partial class frmIndex : Form
+    {
+        public frmIndex()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEx1_Click(object sender, EventArgs e)
+        {
+            frmEx1 ex1 = new frmEx1();
+            ex1.Show();
+        }
+
+        private void btnEx2_Click(object sender, EventArgs e)
+        {
+            frmEx2 ex2 = new frmEx2();
+            ex2.Show();
+        }
+    }
+}
