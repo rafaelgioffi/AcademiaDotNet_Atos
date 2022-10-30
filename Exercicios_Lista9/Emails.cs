057702139e4144ab33fb3314f8cd8e5dc821a3af
Rafael Gioffi <rafaelgioffi@gmail.com>
2022-10-30 09:34:59 -0300

    Uploading remaining Emails class

---
+++ Exercicios_Lista9/Emails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios_Lista9
+{
+    class Emails
+    {
+        private string _email { get; set; }
+        private string _dominio { get; set; }
+
+        public Emails(string email, string dominio)
+        {
+            _email = email;
+            _dominio = dominio;
+        }
+
+        public string Email { get => _email; set => _email = value; }
+        public string Dominio { get => _dominio; set => _dominio = value; }
+    }
+}
