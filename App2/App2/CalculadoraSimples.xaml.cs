2ee1b77b556bf88d653a688a1d5f6b144c81dc3a
Rafael Gioffi <rafaelgioffi@gmail.com>
2022-12-01 22:46:14 -0300

    Pushing 'App2' Xamarin project

---
+++ App2/App2/CalculadoraSimples.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculadoraSimples : ContentPage
+    {
+        public CalculadoraSimples()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            double.TryParse(txtValorA.Text, out val1);
+            double.TryParse(txtValorB.Text, out val2);
+            Button bt = (Button)sender;
+
+            if (bt.Text == "+")
+            {
+                lblCalculo.Text = $"{(val1 + val2)}";
+                lblstatus.Text = "";
+            }
+            if (bt.Text == "-")
+            {
+                lblCalculo.Text = $"{(val1 - val2)}";
+                lblstatus.Text = "";
+            }
+            if (bt.Text == "x")
+            {
+                if (val1 <= 0 || val2 <= 0)
+                {
+                    lblstatus.Text = "Todo número multiplicado por 0, é 0";
+                    lblCalculo.Text = "";
+                }
+                else
+                {
+                    lblCalculo.Text = $"{(val1 * val2)}";
+                    lblstatus.Text = "";
+                }
+            }
+            if (bt.Text == "/")
+            {
+                if (val1 <= 0 || val2 <= 0)
+                {
+                    lblCalculo.Text = "";
+                    lblstatus.Text = "Não é possível dividir qualquer número por 0";
+                }
+                else
+                {
+                    lblCalculo.Text = $"{(val1 / val2):F}";
+                    lblstatus.Text = "";
+                }
+            }
+        }
+
+    }
+}
---
+++ App2/App2/ParOuImpar.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ParOuImpar : ContentPage
+    {
+        public ParOuImpar()
+        {
+            InitializeComponent();
+            btnExecutar.Clicked += BtnExecutar_Clicked;
+            btnIMC.Clicked += BtnIMC_Clicked;
+        }
+
+        private void BtnIMC_Clicked(object sender, EventArgs e)
+        {
+            double peso, altura, imc;
+
+            double.TryParse(txtPeso.Text, out peso);
+            double.TryParse(txtAltura.Text, out altura);
+
+            if(altura > 0 && peso > 0)
+            {
+                imc = peso * 2 / altura;
+                lblResp.Text = $"Seu I.M.C. é de {imc:F}";
+            }
+            else
+            {
+                lblResp.Text = "Informe o peso e altura antes de calcular o I.M.C.";
+            }
+        }
+
+        private void BtnExecutar_Clicked(object sender, EventArgs e)
+        {
+            int n;
+            int.TryParse(txtNumero.Text, out n);
+
+            if (n > 0)
+            {
+                if (n % 2 == 0)
+                {
+                    lblResp.Text = n.ToString() + " é par";
+                }
+                else
+                {
+                    lblResp.Text = n.ToString() + " é ímpar";
+                }
+            }
+            else
+            {
+                lblResp.Text = "Favor informe um valor";
+            }
+        }
+
+    }
+}
